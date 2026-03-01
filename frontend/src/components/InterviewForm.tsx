5b6548b08c7f0705a39d8e95896cf9347f89ddf1
dbpyja-blip <dbpyja@gmail.com>
2026-03-01 20:46:30 +0530

    Update InterviewForm.tsx

--- frontend/src/components/InterviewForm.tsx
+++ frontend/src/components/InterviewForm.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import React, { useState } from "react";
 import { apiUrl } from "@/config";
 import { Button } from "./button/Button";
 
